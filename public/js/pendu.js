2529f2b12910237290d1210f6126383b34426b4a
zerefsoldier <chantebelmarc@gmail.com>
2014-08-10 00:58:57 +0200

    release

--- public/js/pendu.js
+++ public/js/pendu.js
@@ -1,3 +1,35 @@
+function Pendu(mot)
+{
+  var self = this;
+  this.word = mot;
+  this.lettres = null;
+  this.reglesActive = false;
+  this.finPartie = false;
+  
+  this.afficheMasqueRegles = function()
+  {
+    $("#regles_button").click(function()
+	{
+		if (!self.reglesActive){
+			self.reglesActive = true;
+			document.getElementById("regles_vue").setAttribute("class", "vue");
+		}
+		else{
+			self.reglesActive = false;
+			document.getElementById("regles_vue").setAttribute("class", "pas_vue");
+		}
+  };
+  
+  this.genereHTMLMot = function(){
+    var allLettres = "";
+    for (var i = 0, c = this.word.length; i < c; i++){
+      allLettres += "<span class=lettre_non_decouverte >?</span>";
+    }
+    $("#fenetre_jeu").innerHTML = allLettres;
+    this.lettres = $(".lettre_non_decouverte");
+  };
+}
+
 $(document).ready(function(){
 	
 });
