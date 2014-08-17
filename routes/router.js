f4a25677c51052396dc886e9c17a700086aa0913
Lefebvre Arnaud <a.lefebvre@outlook.fr>
2014-08-18 00:39:23 +0200

    Added endpoint to get a word

--- routes/router.js
+++ routes/router.js
@@ -1,5 +1,14 @@
+var fs = require('fs'),
+    words = JSON.parse(fs.readFileSync('./lib/words.json')),
+    penduLib = require('../lib/pendu'),
+    Pendu = new penduLib().init(words);
+
 module.exports = function(app){
-    app.get('/index', function(req, res){
+    app.get('/', function(req, res){
 	res.sendFile('views/index.html', { root: './' });
+    });
+
+    app.get('/words', function(req, res){
+	res.send(Pendu.getWord(req.query));
     });
 };
